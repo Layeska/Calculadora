c59bda1dc5651f2faa238492214af4456068a32e
Ana Juárez <lisa13suarez@gmail.com>
2022-07-28 22:33:13 -0600

    agregando funcionalidades y otro estilo

--- js.js
+++ js.js
@@ -1,49 +1,54 @@
 let display = document.querySelector(".calculator__screen-top");
+let display2 = document.querySelector(".calculator__screen-content");
 let buttons = document.querySelectorAll(".btn");
 
 let value = "";
+let newvalue = "";
 
 function calcular(e) {
     let tecla = e.target.textContent;
 
-    let operadores = ["+", "-", "*", "÷"];
-
     switch(tecla) {
         case "=":
-            display.textContent = eval(value.replace("÷","/"));
+            /*display.textContent = eval(value.replace("÷","/"));*/
+            display2.textContent = eval(value.replace("÷","/"));
             value = eval(value.replace("÷","/"));
             break;
 
-        case "C": 
+        case "CA": 
             display.textContent = "0";
             value = " ";
-            break;
 
-        case "+":
-        case "-":
-        case "/":
-            /*display.textContent = value + "/";
-            value += "/";*/
-            
-        case "*":
-        case ".":
-        case "%":
-            /*value = value.replace(/+ | - | * | ÷ /, " ");
-            //display.textContent = value;
+            display2.textContent = " ";
+            break;
+        
+        case "C":/*
+            display.textContent = "*******";
+            display2.textContent = "Apagado ";
             break;*/
 
+        case "%":
+            let values = [];
+
+            newvalue = value.replace(/[^\d?]/g, " ");
+            const operador = value.replace(/[\d?]/g, " ").trim();
+            values = newvalue.split(" ");
+            let porcentaje = (values[0] * values[1]) / 100;
+            value = `${values[0]}${operador}${porcentaje}`;
+            display.textContent = value;
+            display2.textContent = porcentaje;
+            break;
+
+        case "/":            
+        case "*":
         case "+":
+        case "-":
         default:
             value += tecla;
             display.textContent = value;
     }
-
-    
-
-
 }
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", calcular);
 }
-
