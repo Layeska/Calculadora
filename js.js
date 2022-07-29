016154fba079cdbae80fe7b1ee9e05f512c65d05
Ana Juárez <lisa13suarez@gmail.com>
2022-07-28 19:14:19 -0600

    agregando funciones

--- js.js
+++ js.js
@@ -3,12 +3,47 @@ let buttons = document.querySelectorAll(".btn");
 
 let value = "";
 
-function escuchando(e) {
-    console.log(e.target.value);
-    value += e.target.textContent;
-    display.textContent = value;
+function calcular(e) {
+    let tecla = e.target.textContent;
+
+    let operadores = ["+", "-", "*", "÷"];
+
+    switch(tecla) {
+        case "=":
+            display.textContent = eval(value.replace("÷","/"));
+            value = eval(value.replace("÷","/"));
+            break;
+
+        case "C": 
+            display.textContent = "0";
+            value = " ";
+            break;
+
+        case "+":
+        case "-":
+        case "/":
+            /*display.textContent = value + "/";
+            value += "/";*/
+            
+        case "*":
+        case ".":
+        case "%":
+            /*value = value.replace(/+ | - | * | ÷ /, " ");
+            //display.textContent = value;
+            break;*/
+
+        case "+":
+        default:
+            value += tecla;
+            display.textContent = value;
+    }
+
+    
+
+
 }
 
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", escuchando);
+    buttons[i].addEventListener("click", calcular);
 }
+
